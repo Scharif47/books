521c001caf469dda9c214d5b20cc91cb51712e73
Scharif47 <scharif47@outlook.de>
2024-05-08 20:17:36 +0200

    edit book functionality implemented

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function BookEdit() {
+function BookEdit({ book, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(book.id, title);
+  };
+
   return (
-    <div>BookEdit</div>
-  )
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
-export default BookEdit
+export default BookEdit;
