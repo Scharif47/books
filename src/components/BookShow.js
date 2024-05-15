4bd953e155386a0dc80b12879ca315bf4de8b249
Scharif47 <scharif47@outlook.de>
2024-05-15 13:13:20 +0200

    context refactor

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import BookEdit from "./BookEdit";
+import useBooksContext from "../hooks/use-books-context";
 
 function BookShow({ book, onDelete, onEdit }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useBooksContext();
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
+  const handleSubmit = () => {
     setShowEdit(false);
-    onEdit(id,newTitle);
-  }
+  };
 
   let content = <h3>{book.title}</h3>;
   if (showEdit) {
@@ -24,7 +25,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
   return (
     <div className="book-show">
-    <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
